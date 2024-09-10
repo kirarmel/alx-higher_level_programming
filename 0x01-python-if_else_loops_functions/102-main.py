ea97ffa989cd4f50a5060f2985a8283abce79b34
Kira Armel Kayisire <kirarmel@gmail.com>
2024-09-10 14:26:51 -0700

    added new files

---
+++ 0x01-python-if_else_loops_functions/102-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+print(magic_calculation(2, 3, 4))  # Example test case
+print(magic_calculation(5, 3, 4))  # Example test case
+print(magic_calculation(2, 3, 1))  # Example test case
+print(magic_calculation(1, 2, 3))  # Example test case
+print(magic_calculation(3, 2, 1))  # Example test case
