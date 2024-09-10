62db8bb2d5d5fd55b3baeecb826b7d3d1a24128d
Kira Armel Kayisire <kirarmel@gmail.com>
2024-09-10 14:29:54 -0700

    added new files

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(chr(122 - i) if i % 2 == 0 else chr(90 - i) for i in range(26)), end='')
+print("".join("{}".format(chr(c)) if c % 2 == 0 else "{}".format(chr(c).upper()) for c in range(122, 96, -1)), end="")
