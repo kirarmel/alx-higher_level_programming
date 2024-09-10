b133b03f87700ea2c5daaa906447c1a375bfd5c0
Kira Armel Kayisire <kirarmel@gmail.com>
2024-09-10 14:08:12 -0700

    modified file 8

--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 uppercase = __import__('8-uppercase').uppercase
 
-uppercase("best")
-uppercase("Best School 98 Battery street")
+uppercase("holberton")
+uppercase("Holberton School")
+uppercase("Holberton School, 98 battery street")
+uppercase("")
+uppercase(98)
+uppercase("z")
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
-
 def uppercase(str):
-    print("".join(chr(ord(c) - 32) if ord('a') <= ord(c) <= ord('z') else c for c in str))
+    for c in str:
+    
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)  
+        print("{}".format(c), end="")
+    print("")  
