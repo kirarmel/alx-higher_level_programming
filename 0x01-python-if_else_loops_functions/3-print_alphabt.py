4c0904c2dfbaebd7d3e1c5f8666393c1d03cdf80
Kira Armel Kayisire <kirarmel@gmail.com>
2024-09-10 14:13:39 -0700

    modified a 3, 6, 8 files

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
-print("{}".format(
-    "".join([chr(i) for i in range(97, 123) if i not in (ord('q'), ord('e'))])
-), end="")
+#!/usr/bin/python3
+for c in range(97, 123):
+    if c != 101 and c != 113:
+        print(chr(c), end="")
+print()
